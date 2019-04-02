ffab82c4d8d84704692acfb64200a8e0e6cdce61
alyokhina-o <alyokhina-o@yandex-team.ru>
2019-04-02 17:58:00 +0300

    create mob strategy

---
+++ src/main/java/ru/spb/hse/roguelike/model/strategy/MobStrategy.java
@@ -0,0 +1,11 @@
+package ru.spb.hse.roguelike.model.strategy;
+
+import ru.spb.hse.roguelike.model.map.GameCell;
+import ru.spb.hse.roguelike.model.object.alive.Mob;
+
+import javax.annotation.Nonnull;
+
+public interface MobStrategy {
+    void move(@Nonnull final GameCell[][] map,
+              @Nonnull final Mob mob);
+}
