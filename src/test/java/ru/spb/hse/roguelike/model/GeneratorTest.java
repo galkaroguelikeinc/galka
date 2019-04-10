06c6413ccd2b061d6a0b27dc89298b95800a2791
Marieliseeva <eliseevamary@mail.ru>
2019-04-10 16:46:24 +0300

    change room count test

--- src/test/java/ru/spb/hse/roguelike/model/GeneratorTest.java
+++ src/test/java/ru/spb/hse/roguelike/model/GeneratorTest.java
@@ -44,8 +44,15 @@ public class GeneratorTest {
     }
 
     @Test
-    public void testCountRooms() throws MapGeneratorException {
-        GameModel gameModel = generator.generateModel(3, 20, 25);
+    public void testCountRooms() {
+        GameModel gameModel = null;
+        while (true) {
+            try {
+                gameModel = generator.generateModel(3, 20, 25);
+                break;
+            } catch (MapGeneratorException ignored) {
+            }
+        }
         int actualRoomCount = 0;
         for (int i = 0; i < 25; i++) {
             for (int j = 0; j < 20; j++) {
