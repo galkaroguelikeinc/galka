b6c709b05d16728093377d91099583674da9b4f7
alyokhina-o <alyokhina-o@yandex-team.ru>
2019-04-23 19:45:11 +0300

    fixed room count

--- src/main/java/ru/spb/hse/roguelike/Main.java
+++ src/main/java/ru/spb/hse/roguelike/Main.java
@@ -29,7 +29,7 @@ public class Main {
                     return GameMapCellType.EMPTY;
                 });
             } else {
-                model = generator.generateModel(6, 20, 20);
+                model = generator.generateModel(3, 20, 20);
             }
 
             View view = new TerminalView(model);
